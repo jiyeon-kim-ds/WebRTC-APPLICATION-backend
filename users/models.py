1f082b49ca83c70a802e53949634a76d618e09b0
jiyeon.kim.ds <jiyeon.kim.ds@gmail.com>
2022-02-28 12:08:58 +0900

    Add: User Model

---
+++ users/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db                  import models
+
+class UserManger(BaseUserManager):
+    def create_user(self, email, first_name, last_name ,password):
+        if not email:
+            raise ValueError('email is required')
+        email = self.normalize_email(email)
+        user  = self.model(email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    objects = UserManger()
+
+    email       = models.EmailField(max_length=255, unique=True)
+    first_name  = models.CharField(max_length=30)
+    last_name   = models.CharField(max_length=30)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    update_at   = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD  = 'email'
+    
+    class Meta:
+        db_table='users'
