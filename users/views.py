c998181c01e9fa9c5e944cea43d064330127c1ef
jiyeon.kim.ds <jiyeon.kim.ds@gmail.com>
2022-03-02 14:22:39 +0900

    Add SigInAPIView

--- users/views.py
+++ users/views.py
@@ -1,6 +1,21 @@
-from rest_framework.generics         import CreateAPIView
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserTokenSerializer
 
 class UserSignUpAPIView(CreateAPIView):
     serializer_class = UserSerializer
+
+class UserSignInAPIView(APIView):
+    def post(self, request):
+        serializer = UserTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)        
+        user = serializer.validated_data['user']
+
+        token, is_created = Token.objects.get_or_create(
+            user=user
+        )
+
+        return Response({'token': token.key})
