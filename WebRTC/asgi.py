ede252b78ff0e38a53ce27cb672ef352414d6adb
jiyeon.kim.ds <jiyeon.kim.ds@gmail.com>
2022-03-15 15:30:55 +0900

    [WIP]Add Video call function

--- WebRTC/asgi.py
+++ WebRTC/asgi.py
@@ -6,14 +6,22 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
-
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.auth    import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+import call.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebRTC.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
+
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            call.routing.websocket_urlpatterns
+        )
+    ),
 })
---
+++ call/consumers.py
@@ -0,0 +1,39 @@
+import json
+
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+class VideoCallConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+
+        await self.channel_layer.group_add(
+            self.room_name,
+            self.channel_name
+        )
+
+        self.accept()
+    async def disconnect(self, close_code):
+        pass
+        await self.channel_layer.group_discard(
+            self.room_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        await self.channel_layer.group_send(
+            self.room_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+    async def send_message(self, event):
+        message = event['message']
+
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
